7f4a43cfbee36e2b5591e2aac7f751a40f4d0263
rex.codhes <gamermobile229@gmail.com>
2025-08-18 11:13:53 +0530

    scraper and exa fns

---
+++ backend/src/services/exa.ts
@@ -0,0 +1,69 @@
+import * as dotenv from "dotenv";
+import {Exa} from "exa-js";
+
+dotenv.config();
+
+const exaKey: string | undefined = process.env.EXA_API_KEY;
+if (!exaKey) {
+  throw new Error("Missing EXA_API_KEY in environment variables");
+}
+
+const exa = new Exa(exaKey);
+
+const searchData = {
+  searchTopics: {
+    entities: [
+      "ISRO",
+      "Chandrayaan-4",
+      "Pragyan-2 rover",
+      "Satish Dhawan Space Centre",
+      "Shackleton crater",
+    ],
+    concepts: [
+      "lunar south pole exploration",
+      "water ice on the Moon",
+      "lunar regolith drilling technology",
+      "space mission budget and funding",
+    ],
+    claims: [
+      "The Pragyan-2 rover's drill can penetrate up to 2 meters of lunar regolith.",
+      "The primary objective of the Chandrayaan-4 mission is to search for water ice.",
+      "The mission's official budget is ₹600 crore.",
+      "Final ground tests for the mission have been successfully completed.",
+    ],
+  },
+  ragQuestion:
+    "What are the primary objectives, technical specifications (including the Pragyan-2 rover's capabilities), exploration target (Shackleton crater), and official budget of ISRO's Chandrayaan-4 mission?",
+};
+
+const entities: string[] = searchData.searchTopics.entities;
+const concepts: string[] = searchData.searchTopics.concepts;
+const claims: string[] = searchData.searchTopics.claims;
+
+async function runExa(): Promise<void> {
+  for (let i = 0, j = 0; i < 4 && j < 4; i++, j++) {
+    console.log(searchData.searchTopics.entities[i]);
+    const result = await exa.searchAndContents(
+      `${entities[i]} ${concepts[j]}`,
+      {
+        text: true,
+        type: "auto",
+        numResults: 1,
+        context: true,
+      }
+    );
+    console.log(JSON.stringify(result, null, 2));
+  }
+
+  for (let k = 0, i = 0; i < 4 && k < 4; i++, k++) {
+    const result = await exa.searchAndContents(`${entities[i]} ${claims[k]}`, {
+      text: true,
+      type: "auto",
+      numResults: 1,
+      context: true,
+    });
+    console.log(JSON.stringify(result, null, 2));
+  }
+}
+
+runExa();
---
+++ backend/src/services/scraper.ts
@@ -0,0 +1,107 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+import { chromium } from "playwright";
+import type { Page } from "playwright";
+import { BROWSER_HEADERS } from "../lib/constants.js";
+
+const scrapeUrl =
+  "https://www.republicworld.com/cricket/daava-hai-nahi-hoga-ex-csk-star-recalls-operation-sindoor-claims-india-vs-pakistan-asia-cup-2025-match-will-not-happen";
+
+interface ScrapeResult {
+  title: string;
+  body: string;
+}
+
+async function scrape(scrapeUrl: string): Promise<ScrapeResult | undefined> {
+  const locatorTweet = "div.tweet-content.media-body";
+  const locatorGeneric = "body";
+  let pageData: ScrapeResult;
+  const possibleTwtUrls = [
+    "https://x.com",
+    "https://twitter.com",
+    "https://mobile.twitter.com",
+    "https://mobile.x.com",
+  ];
+
+  try {
+    const isTwtUrl = possibleTwtUrls.some((url) => scrapeUrl.startsWith(url));
+
+    if (isTwtUrl) {
+      const twtId = scrapeUrl.split("/").slice(3).join("/"); // slices the url contents after the domain
+
+      pageData = await scrapeWithPlaywright(
+        `https://nitter.net/${twtId}`,
+        locatorTweet
+      );
+    } else {
+      pageData = await scrapeWithAxios(scrapeUrl);
+
+      if (pageData.body.length < 50) {
+        pageData = await scrapeWithPlaywright(scrapeUrl, locatorGeneric);
+      }
+    }
+
+    console.log("Scraped data:", pageData);
+    return pageData;
+  } catch (err) {
+    console.warn("Axios failed, falling back to Playwright:", err);
+    return undefined;
+  }
+}
+
+async function scrapeWithAxios(url: string): Promise<ScrapeResult> {
+  let title = "";
+  let articleBody = "";
+
+  try {
+    const response = await axios.get(url, {
+      headers: BROWSER_HEADERS,
+    });
+
+    const html: string = response.data;
+    const $ = cheerio.load(html);
+    $("script, style, noscript, svg, footer, header, nav").remove();
+    title = $("h1").text().trim();
+    articleBody = $("body").text().trim().replace(/\s+/g, " ");
+
+    if (!articleBody || articleBody.length < 50) {
+      throw new Error("Axios/cheerio scraping returned empty or invalid body");
+    }
+
+    console.log("--- SCRAPED WITH AXIOS ---");
+    return { title, body: articleBody };
+  } catch (error) {
+    console.error("Error scraping the article:", error);
+    return { title: "Error", body: "Failed to scrape with Axios" };
+  }
+}
+
+async function scrapeWithPlaywright(
+  url: string,
+  locatorVar: string
+): Promise<ScrapeResult> {
+  let pageData: string[] = [];
+
+  try {
+    const browser = await chromium.launch({ headless: false, timeout: 8000 });
+    const context = await browser.newContext();
+    const page: Page = await context.newPage();
+
+    await page.goto(url);
+    await page.waitForLoadState("domcontentloaded");
+    pageData = await page.locator(locatorVar).allInnerTexts();
+    await browser.close();
+
+    console.log("--- SCRAPED WITH PLAYWRIGHT ---");
+  } catch (error) {
+    console.error("An error occurred:", error);
+  } finally {
+    const body = pageData.join(" ").trim();
+    return {
+      title: "",
+      body,
+    };
+  }
+}
+
+scrape(scrapeUrl);
